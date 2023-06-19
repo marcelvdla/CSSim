d4704a4b140c5e4dd58dc51b6f88b637c09c474c
Jared Frazier <cscidev001@gmail.com>
2023-06-19 17:53:02 +0200

    init definition of one forest ecosystem

---
+++ src/one_forest_system.jl
@@ -0,0 +1,41 @@
+using DynamicalSystems
+
+"""
+    one_forest_system(u0; p, f, h)
+
+Coupled ODE system for modeling the dynamics of a single forest ecosystem's
+young and old tree density.
+"""
+function one_forest_system(u0; p, f, h)
+    params = [p, f, h]
+    return CoupledODEs(antonovsky_rule, u0, params)
+end 
+
+"""
+    antonovsky_rule(u, p, t)
+
+Return the density of young trees (`x`) and old trees (`y`) for a one-species,
+two sub-population forest ecosystem. 
+
+[1] : Equation (1) from [Cantin2020](https://www.sciencedirect.com/science/article/pii/S1476945X20300386)
+
+[2] : [Antonosky1990](https://www.sciencedirect.com/science/article/abs/pii/004058099090043U?via%3Dihub)
+"""
+function antonovsky_rule(u, p, t)
+    x, y = u
+    p, f, h = p
+    xdot = ρ*y - γ(y)*x - f*x
+    ydot = f*x - h*y
+    return SVector(xdot, ydot)    
+end 
+
+"""
+    γ(y, a, b, c)
+
+Return mortality rate of young trees
+
+TODO: Define default params
+
+[1] : Equation (2) from [Cantin2020](https://www.sciencedirect.com/science/article/pii/S1476945X20300386)
+"""
+γ(y, a, b, c) = a*(y - b)^2 - c
